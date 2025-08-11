ff37d12b4cdfc629863272e53f21abdc09619483
lojo2 <lojo2@DESKTOP-3N1BOVP>
2025-08-11 13:19:35 -0300

    Sobrecarga

--- product_stock/src/application/Main.java
+++ product_stock/src/application/Main.java
@@ -7,17 +7,18 @@ import entities.Product;
 public class Main {
 
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
-		Product product = new Product(); 
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
-		product.name = sc.nextLine();	
+		String name = sc.nextLine();	
 		System.out.print("Price: ");
-		product.price = sc.nextDouble();	
-		System.out.print("Quantity in stock: ");
-		product.quantity = sc.nextInt();	
+		double price = sc.nextDouble();	
+		
+		/* System.out.print("Quantity in stock: ");
+		int quantity = sc.nextInt(); */	
+		
+		Product product = new Product(name, price); 
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
--- product_stock/src/entities/Product.java
+++ product_stock/src/entities/Product.java
@@ -6,6 +6,23 @@ public class Product {
 	public double price;
 	public int quantity;
 	
+	public Product() {
+		
+	}
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+		// this.quantity = 0; // opcional atribuir = 0 ou usar this.
+
+	}
+	
 	public double totalValueStock() {
 		return price * quantity;
 	}
