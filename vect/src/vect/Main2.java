d6c47e0848d520f64491b1cacd34e6108f83247a
lojo2 <lojo2@DESKTOP-3N1BOVP>
2025-08-15 16:43:03 -0300

    vect

---
+++ vect/src/vect/Main2.java
@@ -0,0 +1,35 @@
+package vect;
+
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		Product[] vect = new Product[n];
+		
+		for (int i=0; i<vect.length; i++) {
+			sc.nextLine();
+			String name = sc.nextLine();
+			double price = sc.nextDouble();
+			
+			vect[i] = new Product(name, price);
+		}
+		
+		double sum = 0;
+		for (int i=0; i<vect.length; i++) {
+			sum += vect[i].getPrice();
+		}
+		double avg = sum / vect.length;
+		
+		System.out.printf("Average Price = %.2f%n", avg);
+		
+		
+		sc.close();
+	}
+
+}
